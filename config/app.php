890d098badfb9b19b978715f4abf26e2d03b64c8
NGOC HA <HA@NGOCs-MacBook-Pro.local>
2018-05-26 15:15:33 +0700

    deploy

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
+        Spatie\Analytics\AnalyticsServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Analytics' => Spatie\Analytics\AnalyticsFacade::class,
 
     ],
 
--- resources/views/layouts/app/app.blade.php
+++ resources/views/layouts/app/app.blade.php
@@ -25,6 +25,16 @@
     <link href="{{ asset('app/assets/css/owl.theme.default.min.css') }}" rel="stylesheet">
     <!-- Theme Style -->
     <link href="{{ asset('app/assets/css/style.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119908639-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-119908639-1');
+    </script>
     
 </head>
 
