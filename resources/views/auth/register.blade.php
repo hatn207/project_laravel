5dd0c4968e5ae3a9daedc0a90adc2e13b6bf79b1
NGOC HA <HA@NGOCs-MacBook-Pro.local>
2018-05-26 11:02:43 +0700

    deploy

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,36 @@
-@extends('layouts.app.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Register</title>
+    
+    <!-- Styles -->
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('theme/common/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="{{ asset('theme/common/metisMenu/metisMenu.min.css') }}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('theme/dist/css/sb-admin-2.css') }}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{ asset('theme/common/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    
+    <!-- jQuery -->
+    <script src="{{ asset('theme/common/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('theme/common/bootstrap/js/bootstrap.min.js') }}"></script>
+    
+    {{--  <link href="{{ asset('css/app.css') }}" rel="stylesheet">  --}}
+</head>
+<body>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -74,4 +104,19 @@
         </div>
     </div>
 </div>
-@endsection
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+
+<!-- jQuery -->
+<script src="{{ asset('theme/common/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('theme/common/bootstrap/js/bootstrap.min.js') }}"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="{{ asset('theme/common/metisMenu/metisMenu.min.js') }}"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="{{ asset('theme/dist/js/sb-admin-2.js') }}"></script>
+</body>
+</html>
