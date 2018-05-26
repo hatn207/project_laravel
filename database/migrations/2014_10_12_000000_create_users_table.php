2e45f2a4365c2bb4616282bd477ed42db6b92e15
NGOC HA <HA@NGOCs-MacBook-Pro.local>
2018-05-26 11:11:15 +0700

    deploy

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('group');
+            // $table->integer('group');
             $table->rememberToken();
-            $table->string('api_token');
+            // $table->string('api_token');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
