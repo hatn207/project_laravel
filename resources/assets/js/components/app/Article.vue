d82b74cec715778f4775625f9fdc04dc72946d60
NGOC HA <HA@NGOCs-MacBook-Pro.local>
2018-05-27 14:30:48 +0700

    deploy

--- resources/assets/js/components/app/Article.vue
+++ resources/assets/js/components/app/Article.vue
@@ -161,7 +161,15 @@
     </div>
     <!--Left Section-->
 </template>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119908639-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
+gtag('config', 'UA-119908639-1');
+</script>
 <script>
 export default {
     data: function () {
--- resources/views/layouts/app/app.blade.php
+++ resources/views/layouts/app/app.blade.php
@@ -265,15 +265,7 @@
 <!-- Theme Script -->
 <script src="{{ asset('app/assets/js/script.js') }}"></script>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119908639-1"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', 'UA-119908639-1');
-</script>
 </body>
 
 </html>
